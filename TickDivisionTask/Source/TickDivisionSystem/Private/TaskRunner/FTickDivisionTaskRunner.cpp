517f99c8c5e74f12da8e1fe7bf80b7f4c6219d96
DashLee <lsj7915@naver.com>
2022-02-07 00:06:09 +0900

    Add TaskRunner

---
+++ TickDivisionTask/Source/TickDivisionSystem/Private/TaskRunner/FTickDivisionTaskRunner.cpp
@@ -0,0 +1,93 @@
+﻿#include "FTickDivisionTaskRunner.h"
+
+#include "TickDivisionDefine.h"
+#include "Interface/TickDivisionTaskInterface.h"
+
+FTickDivisionTaskRunner::FTickDivisionTaskRunner(float InLimitTime, TArray<TSharedPtr<ITickDivisionTask>>& InTaskContainer, FTickDivisionSystemDelegate& InDelegate)
+	: m_Index(0)
+	, m_LimitTime(InLimitTime)
+	, m_TaskContainer(InTaskContainer)
+	, m_Delegate(InDelegate)
+{
+}
+
+void FTickDivisionTaskRunner::Run()
+{
+	int32 LocalLoopCount = m_TaskContainer.Num();
+	for(m_Index = 0 ; m_Index < LocalLoopCount ; ++m_Index)
+	{
+		TSharedPtr<ITickDivisionTask> LocalTask = m_TaskContainer[m_Index]; 
+		RunTask(LocalTask);
+
+		if(IsOverLimitTime())
+			break;
+	}
+}
+
+bool FTickDivisionTaskRunner::IsRemainLimitTime() const
+{
+	return m_LimitTime > 0.0f;
+}
+
+bool FTickDivisionTaskRunner::IsOverLimitTime() const
+{
+	return m_LimitTime <= 0.0f;
+}
+
+void FTickDivisionTaskRunner::RunTask(TSharedPtr<ITickDivisionTask> InTask)
+{
+	if(false == InTask.IsValid())
+		return;
+
+	do
+	{
+		double LocalTaskStartTime = FPlatformTime::Seconds();
+
+		FTickDivisionTaskStatus LocalResultStatus = InTask->Tick(GetLimitTime());
+
+		const double LocalTaskEllapsedTime = FPlatformTime::Seconds() - LocalTaskStartTime;
+		MinusLimitTime(static_cast<float>(LocalTaskEllapsedTime));
+		
+		if(LocalResultStatus.IsWaiting())
+			break;
+		
+		if(LocalResultStatus.IsEndTask())
+		{
+			OnTaskEndEvent(InTask, LocalResultStatus);
+			break;
+		}
+		
+	}while(IsRemainLimitTime());
+}
+
+float FTickDivisionTaskRunner::GetLimitTime() const
+{
+	return m_LimitTime;
+}
+
+void FTickDivisionTaskRunner::MinusLimitTime(float InMinusTime)
+{
+	m_LimitTime -= InMinusTime;
+}
+
+void FTickDivisionTaskRunner::OnTaskEndEvent(TSharedPtr<ITickDivisionTask> InTask, ETickDivisionTaskStatus InTaskStatus)
+{
+	m_Delegate.ExecuteIfBound(InTaskStatus, InTask);
+	m_TaskContainer.RemoveSingle(InTask);
+	--m_Index;
+}
+
+FTickDivisionTaskUnlimitRunner::FTickDivisionTaskUnlimitRunner(TArray<TSharedPtr<ITickDivisionTask>>& InTaskContainer, FTickDivisionSystemDelegate& InDelegate)
+	: FTickDivisionTaskRunner(0.0f, InTaskContainer, InDelegate)
+{
+}
+
+bool FTickDivisionTaskUnlimitRunner::IsRemainLimitTime() const
+{
+	return true;
+}
+
+bool FTickDivisionTaskUnlimitRunner::IsOverLimitTime() const
+{
+	return false;
+}
---
+++ TickDivisionTask/Source/TickDivisionSystem/Private/TaskRunner/FTickDivisionTaskRunner.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "TickDivisionDefine.h"
+
+class ITickDivisionTask;
+
+class FTickDivisionTaskRunner
+{
+public:
+	FTickDivisionTaskRunner(float InLimitTime, TArray<TSharedPtr<ITickDivisionTask>>& InTaskContainer, FTickDivisionSystemDelegate& InDelegate);
+	virtual ~FTickDivisionTaskRunner() = default;
+	
+	void Run();
+
+protected:
+	virtual bool IsRemainLimitTime() const;
+	virtual bool IsOverLimitTime() const;
+	
+private:
+	void RunTask(TSharedPtr<ITickDivisionTask> InTask);
+	float GetLimitTime() const;
+	void MinusLimitTime(float InMinusTime);
+	void OnTaskEndEvent(TSharedPtr<ITickDivisionTask>, ETickDivisionTaskStatus InTaskStatus);
+	
+private:
+	int32 m_Index;
+	float m_LimitTime;
+	TArray<TSharedPtr<ITickDivisionTask>>& m_TaskContainer;
+	FTickDivisionSystemDelegate& m_Delegate;
+};
+
+class FTickDivisionTaskUnlimitRunner final : public FTickDivisionTaskRunner
+{
+public:
+	FTickDivisionTaskUnlimitRunner(TArray<TSharedPtr<ITickDivisionTask>>& InTaskContainer, FTickDivisionSystemDelegate& InDelegate);
+	
+protected:
+	virtual bool IsRemainLimitTime() const override;
+	virtual bool IsOverLimitTime() const override;
+};
