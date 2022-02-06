790bde4e4c6b122d7d672a86a81e73f3e15644c8
DashLee <lsj7915@naver.com>
2022-02-07 00:04:56 +0900

    Change Enum, Add FTickDivisionTaskStatus, Add parameter in FTickDivisionSystemDelegate

--- TickDivisionTask/Source/TickDivisionSystem/Public/TickDivisionDefine.h
+++ TickDivisionTask/Source/TickDivisionSystem/Public/TickDivisionDefine.h
@@ -6,7 +6,7 @@ enum class ETickDivisionTaskStatus
 {
 	None = 0,
 
-	Continue,
+	Tasking,
 	Waiting,
 	Succeed,
 	Failed,
@@ -14,4 +14,26 @@ enum class ETickDivisionTaskStatus
 	Max
 };
 
-DECLARE_DELEGATE(FTickDivisionSystemDelegate)
+class FTickDivisionTaskStatus
+{
+public:
+	FTickDivisionTaskStatus(const ETickDivisionTaskStatus InStatus)
+		: m_Status(InStatus)
+	{
+		
+	}
+
+	bool IsWaiting() const { return m_Status == ETickDivisionTaskStatus::Waiting; }
+	bool IsEndTask() const { return m_Status == ETickDivisionTaskStatus::Succeed || m_Status == ETickDivisionTaskStatus::Failed; }
+
+	operator ETickDivisionTaskStatus() const
+	{
+		return m_Status;
+	}
+
+private:
+	const ETickDivisionTaskStatus m_Status;
+};
+
+class ITickDivisionTask;
+DECLARE_DELEGATE_TwoParams(FTickDivisionSystemDelegate, ETickDivisionTaskStatus, TSharedPtr<ITickDivisionTask>)
