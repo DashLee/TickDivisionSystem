6bbb6fa6a72f02931852610842862533678bea9f
DashLee <lsj7915@naver.com>
2022-02-07 00:05:45 +0900

    Write TickDivisionSystemInterface

--- TickDivisionTask/Source/TickDivisionSystem/Public/Interface/TickDivisionSystemInterface.h
+++ TickDivisionTask/Source/TickDivisionSystem/Public/Interface/TickDivisionSystemInterface.h
@@ -2,7 +2,9 @@
 
 #include "TickDivisionDefine.h"
 
-class ITickDivisionSystem
+class ITickDivisionTask;
+
+class TICKDIVISIONSYSTEM_API ITickDivisionSystem
 {
 public:
 	virtual ~ITickDivisionSystem() = default;
@@ -11,4 +13,14 @@ public:
 	virtual void SetDelegate(FTickDivisionSystemDelegate InDelegate) PURE;
 	virtual void DeleteDelegate() PURE;
 	virtual void Shutdown() PURE;
+
+	virtual void AddReferencedObjects(FReferenceCollector& Collector) PURE;
+	virtual void Tick(float InTimeLimit) PURE;
+
+	virtual void AddTask(TSharedPtr<ITickDivisionTask> InTask) PURE;
+	virtual bool RemoveTask(TSharedPtr<ITickDivisionTask> InTask) PURE;
+	virtual void Flush() PURE;
+	virtual void ClearTask() PURE;
+	
+	virtual void ExecuteBound(const ETickDivisionTaskStatus& InType, TSharedPtr<ITickDivisionTask> InTask) PURE;
 };
