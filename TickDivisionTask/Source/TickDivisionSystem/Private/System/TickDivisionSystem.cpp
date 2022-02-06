eea70922066841cca15021053b777555c710dd9f
DashLee <lsj7915@naver.com>
2022-02-07 00:06:31 +0900

    Write TickDivisionSystem

--- TickDivisionTask/Source/TickDivisionSystem/Private/System/TickDivisionSystem.cpp
+++ TickDivisionTask/Source/TickDivisionSystem/Private/System/TickDivisionSystem.cpp
@@ -1,5 +1,8 @@
 #include "TickDivisionSystem.h"
 
+#include "Interface/TickDivisionTaskInterface.h"
+#include "TaskRunner/FTickDivisionTaskRunner.h"
+
 TSharedPtr<ITickDivisionSystem> ITickDivisionSystem::Create()
 {
 	return MakeShareable(new FTickDivisionSystem);
@@ -12,11 +15,52 @@ void FTickDivisionSystem::SetDelegate(FTickDivisionSystemDelegate InDelegate)
 
 void FTickDivisionSystem::DeleteDelegate()
 {
-	SetDelegate(nullptr);
+	m_Delegate.Unbind();
 }
 
 void FTickDivisionSystem::Shutdown()
 {
 	// to do release process
 	DeleteDelegate();
+	m_TaskContainer.Reset();
+}
+
+void FTickDivisionSystem::AddReferencedObjects(FReferenceCollector& Collector)
+{
+	for(TSharedPtr<ITickDivisionTask> Task : m_TaskContainer)
+		Task->AddReferencedObjects(Collector);
+}
+
+void FTickDivisionSystem::Tick(float InTimeLimit)
+{
+	FTickDivisionTaskRunner(InTimeLimit, m_TaskContainer, m_Delegate).Run();
+}
+
+void FTickDivisionSystem::AddTask(TSharedPtr<ITickDivisionTask> InTask)
+{
+	check (InTask.IsValid() == true);
+	check (m_TaskContainer.Contains(InTask) == false);
+
+	m_TaskContainer.AddUnique(InTask);
+}
+
+bool FTickDivisionSystem::RemoveTask(TSharedPtr<ITickDivisionTask> InTask)
+{
+	return m_TaskContainer.RemoveSingle(InTask) > 0 ? true : false;
+}
+
+void FTickDivisionSystem::Flush()
+{
+	FTickDivisionTaskUnlimitRunner(m_TaskContainer, m_Delegate).Run();
+}
+
+void FTickDivisionSystem::ClearTask()
+{
+	if(m_TaskContainer.Num() > 0)
+		m_TaskContainer.Reset();
+}
+
+void FTickDivisionSystem::ExecuteBound(const ETickDivisionTaskStatus& InType, TSharedPtr<ITickDivisionTask> InTask)
+{
+	m_Delegate.ExecuteIfBound(InType, InTask);
 }
--- TickDivisionTask/Source/TickDivisionSystem/Private/System/TickDivisionSystem.h
+++ TickDivisionTask/Source/TickDivisionSystem/Private/System/TickDivisionSystem.h
@@ -9,8 +9,19 @@ protected:
 	virtual void SetDelegate(FTickDivisionSystemDelegate InDelegate) override;
 	virtual void DeleteDelegate() override;
 	virtual void Shutdown() override;
+
+	virtual void AddReferencedObjects(FReferenceCollector& Collector) override;
+	virtual void Tick(float InTimeLimit) override;
+
+	virtual void AddTask(TSharedPtr<ITickDivisionTask> InTask) override;
+	virtual bool RemoveTask(TSharedPtr<ITickDivisionTask> InTask) override;
+	virtual void Flush() override;
+	virtual void ClearTask() override;
+	
+	virtual void ExecuteBound(const ETickDivisionTaskStatus& InType, TSharedPtr<ITickDivisionTask> InTask) override;
 	// ~~End ITickDivisionSystem Interface
 
 private:
 	FTickDivisionSystemDelegate m_Delegate;
+	TArray<TSharedPtr<ITickDivisionTask>> m_TaskContainer;
 };
