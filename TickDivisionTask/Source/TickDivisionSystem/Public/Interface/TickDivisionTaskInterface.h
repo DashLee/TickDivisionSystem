5e885e67ac416a13aa26789de76a2520ab2c0ce7
DashLee <lsj7915@naver.com>
2022-02-07 00:05:58 +0900

    Write TickDivisionTaskInterface

--- TickDivisionTask/Source/TickDivisionSystem/Public/Interface/TickDivisionTaskInterface.h
+++ TickDivisionTask/Source/TickDivisionSystem/Public/Interface/TickDivisionTaskInterface.h
@@ -1,7 +1,17 @@
 #pragma once
 
-class ITickDivisionTask
+#include "TickDivisionDefine.h"
+
+class TICKDIVISIONSYSTEM_API ITickDivisionTask
 {
 public:
-	
+	virtual ~ITickDivisionTask() = default;
+
+	virtual void AddReferencedObjects(FReferenceCollector& OutCollector) {}
+
+	virtual ETickDivisionTaskStatus Tick(float InTimeLimit) PURE;
+
+	virtual void NextStep() PURE;
+	virtual void Succeed() PURE;
+	virtual void Failed() PURE;
 };
